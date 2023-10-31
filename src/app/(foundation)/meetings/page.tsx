d417911138510d7299692bc30b84b3cf21de6b05
“Jonathan” <jvardy@risd.edu>
2023-10-31 12:37:22 -0700

    fix: meetings page

--- src/app/(foundation)/meetings/page.tsx
+++ src/app/(foundation)/meetings/page.tsx
@@ -8,29 +8,31 @@ export const metadata: Metadata = {
 
 export default function IndexPage() {
   return (
-    <Container className="w-full max-w-[800px]">
-      <div>
-        <h1>Meetings</h1>
-            <p className="mt-6">
-              {`We host weekly in-person discussions led by artists, providing a space 
+    <>
+      <Container className="my-6 w-full max-w-[800px]">
+        <div>
+          <h1>Meetings</h1>
+          <p>
+            {`We host weekly in-person discussions led by artists, providing a space 
               for creative collaboration and dialogue. These meetings offer members the 
               opportunity to connect and engage in meaningful dialogue about their work, 
               ideas, and projects. They are designed to foster a supportive and inclusive environment 
               where members can share their experiences and learn from one another.`}
-            </p>
-        <iframe
-          src="https://lu.ma/embed/calendar/cal-0Gt4yRZeQakRw8M/events?lt=light"
-          className="h-[65vh] w-full max-w-[800px] rounded-md border border-slate-200"
-          allowFullScreen={true}
-          aria-hidden="false"
-          tabIndex={parseInt("0")}
-        />
-      </div>
-    </Container>
-    <div className="mb-8 mt-auto max-w-[800px]">
+          </p>
+          <iframe
+            src="https://lu.ma/embed/calendar/cal-0Gt4yRZeQakRw8M/events?lt=light"
+            className="h-[55vh] w-full max-w-[800px] rounded-md border border-slate-200"
+            allowFullScreen={true}
+            aria-hidden="false"
+            tabIndex={parseInt("0")}
+          />
+        </div>
+      </Container>
+      <div className="mt-8 max-w-[800px]">
         <p className="font-serif text-lg/8 tracking-[.035rem] text-slate-800">
           {`We understand that dialogue is what drives societal transformation and catalyzes profound progress.`}
         </p>
       </div>
+    </>
   );
 }
