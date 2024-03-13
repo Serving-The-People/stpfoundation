c7df7208bd69b358ba9eb66402778f76e6e5304f
feareatsthesoil <105951475+feareatsthesoil@users.noreply.github.com>
2024-03-13 11:18:49 -0400

    Update page.tsx

--- src/app/(restOfApp)/store/page.tsx
+++ src/app/(restOfApp)/store/page.tsx
@@ -1,6 +1,5 @@
 import ThemeComponent from "@/components/ThemeComponent";
 import ShopifyComponent from "@/components/foundation/store/ShopifyComponent";
-import logo from "@/images/STP.Foundation.Logo.svg";
 import { Metadata } from "next";
 import Image from "next/image";
 
@@ -13,12 +12,6 @@ export default function Store() {
   return (
     <div className="flex w-full flex-col place-content-center">
       <ThemeComponent color="#fff" />
-      <Image
-        src={logo}
-        alt="Serving the People Flower"
-        width={70}
-        className="absolute left-0 right-0 top-4 ml-auto mr-auto md:static md:mt-5 md:self-center"
-      />
       <div className="mb-4 mt-24 md:my-4">
         <ShopifyComponent />
       </div>
